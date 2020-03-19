1e34d9a916919110f108767b332ef020a11326e9
balaji38738 <bijjapwar123@gmail.com>
2020-03-20 01:54:24 +0530

    Taken inputs from the user

---
+++ CompsSorting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+#Problem Statement:- Sort the results of arithmetic expressions in ascending and descending order
+#Author:-Balaji Ijjapwar
+#Date:- 20 March 2020
+
+read -p "Enter three numbers: " number1 number2 number3
