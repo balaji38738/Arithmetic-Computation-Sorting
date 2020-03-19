1206c788832f79d7bbd818df5a257e1c52b1b987
balaji38738 <bijjapwar123@gmail.com>
2020-03-20 03:10:59 +0530

    Second expression solved

--- CompsSorting.sh
+++ CompsSorting.sh
@@ -9,12 +9,14 @@ read -p "Enter three numbers: " number1 number2 number3
 #Function to solve the expressions
 function solveExpressions() {
 	result1=`echo "scale=4; $number1 + $number2 * $number3" | bc`
+	result2=`echo "scale=4; $number1 * $number2 + $number3" | bc`
 }
 
 #Function to print results of expressions
 function printResults() {
 	printf "Expressions' results\n"
 	printf "$number1 + $number2 * $number3 = $result1\n"
+	printf "$number1 * $number2 + $number3 = $result2\n"
 }
 
 solveExpressions
